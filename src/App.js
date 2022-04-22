3c113dd7e88f8de0a4a8c20d87e04df1e9d894b1
Leonel Ramirez <leonel_ramirez93@hotmail.com.ar>
2022-04-22 15:19:23 -0300

    uploadin project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import {Formik, Form} from 'formik'
+import *as Yup from 'yup'
+import Input from './components/Input'
+import Button from './components/Button'
+import Container from './components/Container'
+import Section from './components/Section'
+import Balance from './components/Balance'
 
+const compoundInterest = (deposit, contribution,years,rate) =>{
+  let total=deposit
+
+  for(let i=0; i<years; i++){
+   total = (total + contribution) * (rate+1)
+  }
+  return Math.round(total)
+}
+const formatter= new Intl.NumberFormat('en-US',{
+  style:'currency',
+  currency:'USD',
+  minimumFractionDigits:2,
+  maximumFractionDigits:2,
+})
 function App() {
+  const [balance, setBalance] = useState('')
+  const handleSubmit = ({ deposit, contribution, years, rate }) => {
+    const val = compoundInterest(Number(deposit), Number(contribution), Number(years), Number(rate))
+    setBalance(formatter.format(val))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <Section>
+        <Formik
+        initialValues={{
+          deposit:'',
+          contribution:'',
+          years:'',
+          rate:'',
+        }}
+        onSubmit={handleSubmit}
+        validationSchema={Yup.object({
+          deposit: Yup.number().required('Obligatory').typeError('Must be a number'),
+          contribution: Yup.number().required('Obligatory').typeError('Must be a number'),
+          years: Yup.number().required('Obligatory').typeError('Must be a number'),
+          rate: Yup.number().required('Obligatory').typeError('Must be a number').min(0).max(4),
+        })}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Form>
+            <Input name="deposit" label="Initial deposit"/>
+            <Input name="contribution" label="Anual contribution"/>
+            <Input name="years" label="Years"/>
+            <Input name="rate" label="Stimate rate"/>
+            <Button type="submit">Calculate</Button>
+          </Form>
+        </Formik>
+        {balance !=='' ? <Balance>Final balance: {balance}</Balance>:null}
+      </Section>
+    </Container>
+  )
 }
 
-export default App;
+export default App
